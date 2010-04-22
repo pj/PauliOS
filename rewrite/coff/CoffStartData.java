420811c4669c6bb0e40cbf9dd0d3d081c7897d21
Paul Johnson <pauljohnson@localhost.(none)>
2010-04-22 22:09:48 +1200

    blah

--- rewrite/coff/CoffStartData.java
+++
@@ -1,8 +0,0 @@
-package coff;
-
-public class CoffStartData {
-	public int initialPC;
-	public int initialSP;
-	public int argc;
-	public int argv;
-}
--- rewrite/kernel/OpenFile.java
+++
@@ -1,9 +0,0 @@
-package kernel;
-
-import java.io.File;
-
-public class OpenFile {
-	public int process;
-	
-	public File file;
-}
